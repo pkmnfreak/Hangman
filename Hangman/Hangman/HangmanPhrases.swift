21cd6932a325d9ab458c5599807250e084a67768
pkmnfreak <evcchang@berkeley.edu>
2017-02-27 01:38:03 -0800

    Finished Proj1

--- Hangman/Hangman/HangmanPhrases.swift
+++ Hangman/Hangman/HangmanPhrases.swift
@@ -11,6 +11,14 @@ import Foundation
 class HangmanPhrases {
     
     var phrases : NSArray!
+    var letters : [Character] = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+    var display = ""
+    var firstDisplay = ""
+    var phrase = ""
+    var letterGuessed: Character = " "
+    var incorrectGuesses = ""
+    var currentGuess = 1
+    var gameFreeze = false
     
     // Initialize HangmanPhrase with an array of all possible phrases of the Hangman game
     init() {
@@ -24,4 +32,73 @@ class HangmanPhrases {
         return phrases.object(at: index) as! String
     }
     
+    //Spaces out phrase so that display and phrase indices match
+    func spaceOutPhrase(string: String) {
+        for character in string.characters {
+            phrase += String(character) + " "
+        }
+    }
+    
+    //Takes in phrase and creates initial display
+    func updateDisplay(string: String) -> String {
+        for character in string.characters {
+            if String(character) == " " {
+                display += "  "
+            } else {
+                display += "- "
+            }
+        }
+        self.spaceOutPhrase(string: string)
+        firstDisplay = display
+        return display
+    }
+    
+    //Store most recent letter pressed
+    func changeGuessedLetter(character: Character) {
+        letterGuessed = character
+    }
+    
+    //Takes in character of letter pressed and updates display
+    func letterGuessed(character: Character) -> String {
+        print("User pressed \(character)")
+        if !(incorrectGuesses.contains(String(character))) {
+            if phrase.contains(String(character)){
+                let arrayPhrase = Array(phrase.characters)
+                var arrayDisplay = Array(display.characters)
+                for i in 0 ..< arrayPhrase.count {
+                    if arrayPhrase[i] == character {
+                        arrayDisplay[i] = arrayPhrase[i]
+                    }
+                }
+                display = String(arrayDisplay)
+                return display
+            } else {
+                var arrayIncorrect = Array(incorrectGuesses.characters)
+                arrayIncorrect.append(character)
+                incorrectGuesses = String(arrayIncorrect)
+                currentGuess += 1
+                return display
+            }
+        }
+        return display
+    }
+    
+    func reset() {
+        display = firstDisplay
+        letterGuessed = " "
+        incorrectGuesses = ""
+        currentGuess = 1
+        gameFreeze = false
+    }
+    
+    func restart() {
+        display = ""
+        firstDisplay = ""
+        phrase = ""
+        letterGuessed = " "
+        incorrectGuesses = ""
+        currentGuess = 1
+        gameFreeze = false
+    }
+
 }
--- Hangman/Hangman/HangmanViewController.swift
+++ Hangman/Hangman/HangmanViewController.swift
@@ -9,22 +9,66 @@
 import UIKit
 
 class HangmanViewController: UIViewController {
-
+    
+    var hangman = HangmanPhrases()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        let hangmanPhrases = HangmanPhrases()
+        let phrase: String = hangman.getRandomPhrase()
         // Generate a random phrase for the user to guess
-        let phrase: String = hangmanPhrases.getRandomPhrase()
-        print(phrase)
+        Phrase.text = hangman.updateDisplay(string: phrase)
     }
 
+    @IBOutlet weak var ImageView: UIImageView!
+    
+    @IBOutlet weak var Phrase: UILabel!
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
 
+    @IBOutlet weak var IncorrectGuessBox: UILabel!
+    
+    @IBOutlet weak var GuessBox: UILabel!
+    
+    @IBAction func letterWasPressed(_ sender: UIButton) {
+        if !hangman.gameFreeze {
+            GuessBox.text = "Guess: \(hangman.letters[sender.tag])"
+            hangman.changeGuessedLetter(character: hangman.letters[sender.tag])
+        }
+    }
+
+    @IBAction func guessWasPressed(_ sender: UIButton) {
+        if !hangman.gameFreeze {
+            Phrase.text = hangman.letterGuessed(character: hangman.letterGuessed)
+            IncorrectGuessBox.text = "Incorrect Guesses: \(hangman.incorrectGuesses)"
+            ImageView.image = UIImage(named: "hangman\(hangman.currentGuess)")
+            if hangman.currentGuess == 7 {
+                hangman.gameFreeze = true
+                gameOverAlert()
+            } else if hangman.phrase == hangman.display {
+                hangman.gameFreeze = true
+                winAlert()
+            }
+        }
+    }
+    
+    @IBAction func startOverWasPressed(_ sender: UIBarButtonItem) {
+        hangman.reset()
+        Phrase.text = hangman.display
+        ImageView.image = UIImage(named: "hangman\(hangman.currentGuess)")
+        IncorrectGuessBox.text = "Incorrect Guesses: \(hangman.incorrectGuesses)"
+    }
+    
+    @IBAction func newGameWasPressed(_ sender: UIBarButtonItem) {
+        hangman.restart()
+        let phrase: String = hangman.getRandomPhrase()
+        // Generate a random phrase for the user to guess
+        Phrase.text = hangman.updateDisplay(string: phrase)
+        ImageView.image = UIImage(named: "hangman\(hangman.currentGuess)")
+        IncorrectGuessBox.text = "Incorrect Guesses: \(hangman.incorrectGuesses)"
+    }
     /*
     // MARK: - Navigation
 
@@ -34,5 +78,30 @@ class HangmanViewController: UIViewController {
         // Pass the selected object to the new view controller.
     }
     */
-
+    
+    func gameOverAlert() {
+        let alertController = UIAlertController(title: "Game Over", message: "Rip AF", preferredStyle: .alert)
+        
+        // Create the actions
+        let OKAction = UIAlertAction(title: "OK", style: UIAlertActionStyle.default)
+        
+        // Add the actions
+        alertController.addAction(OKAction)
+        
+        // Present the controller
+        self.present(alertController, animated: true, completion: nil)
+    }
+    
+    func winAlert() {
+        let alertController = UIAlertController(title: "You Win!!", message: "You're cool", preferredStyle: .alert)
+        
+        // Create the actions
+        let OKAction = UIAlertAction(title: "OK", style: UIAlertActionStyle.default)
+        
+        // Add the actions
+        alertController.addAction(OKAction)
+        
+        // Present the controller
+        self.present(alertController, animated: true, completion: nil)
+    }
 }
